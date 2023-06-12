9b32b68b29b2eac24f76a97c02e368f8dc91ede3
ant <1006248030@qq.com>
2023-06-12 21:36:53 +0800

    第一次提交

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false // 暂时关闭代码检测
 })
